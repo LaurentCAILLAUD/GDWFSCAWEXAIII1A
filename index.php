1795e38e35ba8fbf77555a68a6d9aa2d8a47368d
LaurentCAILLAUD <laurent.caillaud@icloud.com>
2024-05-05 16:50:38 +0200

    Commit after creation of application welcome pages (css and php)

--- index.php
+++ index.php
@@ -1,16 +1,41 @@
-<?php
-?>
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GDWFSCAWEXAIII1A - Accueil</title>
+    <title>Espace administration - Accueil</title>
+    <link rel="stylesheet" href="css/indexViewStyle.css">
 </head>
 
 <body>
-    <a href="view/admin/loginView.php"><img src="assets/pictures/admin.png" alt="Icône administration"></a>
+    <!-- mainWrapper block start -->
+    <div id="mainWrapper">
+        <!-- navContent block start -->
+        <div id="navContent">
+            <a href="view/admin/loginView.php"><img src="assets/pictures/admin.png" alt="Icône administration"></a>
+        </div>
+        <!-- navContent block end -->
+        <!-- mainContainer block start -->
+        <div id="mainContainer">
+            <div id="logoContent">
+                <img src="assets/pictures/logo-studi.png" alt="Logo STUDI">
+            </div>
+            <div id="welcomeMessageContent">
+                <p>Bienvenue sur l'évaluation d'entrainement</p>
+                <p>Créer une application web</p>
+                <p>GDWFSCAWEXAIII1A</p>
+                <p>Cette application permet de gérer les données du KGB.</p>
+                <p>Pour lister les différentes missions du KGB, cliquez sur le bouton ci-dessous "Voir les missions".</p>
+                <p>Pour administrer les données de cette application, veuillez cliquer sur l'îcone en haut à droite de cette page.</p>
+            </div>
+            <div id="missionButton">
+                <p><a href="view/missionFrontListView.php">Voir les missions</a></p>
+            </div>
+        </div>
+        <!-- mainContainer block end -->
+    </div>
+    <!-- mainWrapper block end -->
 </body>
 
 </html>
