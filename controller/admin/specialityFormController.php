13952470ca4d43b304e2ccc2e3f834ae94caaa6a
LaurentCAILLAUD <laurent.caillaud@icloud.com>
2024-05-27 16:18:59 +0200

    Commit after several modifications in controller files (modification path)

--- controller/admin/specialityFormController.php
+++ controller/admin/specialityFormController.php
@@ -1,7 +1,7 @@
 <?php
 // J'appelle les classes dont je vais avoir besoin:
 require_once('../../Model/Speciality.php');
-require_once('../../Model/SpecialityRepository.php');
+// require_once('../../Model/SpecialityRepository.php');
 // Pour des raisons de sécurité je souhaite vérifier si l'utilisateur qui souhaite afficher cette page est bien connecté. Pour cela je vais avoir besoin d'utiliser le systeme de session donc je commence par le démarrer:
 session_start();
 // Je vais maintenant vérifier que l'utilisateur souhaitant afficher cette page est bien autorisé à le faire. Si ce n'est pas le cas, je redirige ce dernier vers la page de connexion, sinon le script continue:
